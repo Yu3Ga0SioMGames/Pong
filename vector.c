1d624f9899d1b51a748820dc6ed90e4d16fa6a26
Yu3Ga0SioMGames <yu3ga0siom@gmail.com>
2024-05-09 20:55:49 +0300

    Added new func in vector.h and vector.c: vector_multi(Vector, float); vector_normal(Vector); vector_module(Vector);

--- vector.c
+++ vector.c
@@ -1,3 +1,5 @@
+#include <math.h>
+
 #include "vector.h"
 #include "collision.h"
 
@@ -9,4 +11,32 @@ Vector vector_summ(Vector first, Vector second)
     };
 
     return result;
+}
+
+// vector_multiplication
+Vector vector_multi(Vector v0, float n)
+{
+    Vector v1 = {
+        v0.x * n;
+        v0.y * n;
+    };
+
+    return v1;
+}
+
+Vector vector_module(Vector v0)
+{
+    return sqrt(v0.x * v0.x + v0.y * v0.y); // after... remake sqrt
+}
+
+// vector_normalization
+Vector vector_normal(Vector v0)
+{
+    Vector vabs = vector_module(v0);
+    Vector vN = {
+        v0.x / vabs;
+        v0.y / vabs;
+    };
+
+    return vN;
 }
--- vector.h
+++ vector.h
@@ -7,4 +7,10 @@ typedef Point Vector;
 
 Vector vector_summ(Vector, Vector);
 
+Vector vector_multi(Vector, float); // vector_multiplication
+
+Vector vector_module(Vector);
+
+Vector vector_normal(Vector); // vector_normalization
+
 #endif
